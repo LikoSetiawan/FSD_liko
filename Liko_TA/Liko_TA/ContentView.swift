9ec4847ddd3341b6313a47c037f8de2c88bc1fda
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2023-03-26 16:48:28 +0700

    connect firebase
    add project to firebase

--- Liko_TA/Liko_TA/ContentView.swift
+++ Liko_TA/Liko_TA/ContentView.swift
@@ -6,14 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 struct ContentView: View {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, test 1 !")
+            Text("Hello, test 212 !")
         }
         .padding()
     }
